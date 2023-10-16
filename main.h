2eed97953debf1849d960fcc9ef2a963d11e1a04
sophie-analytics <sonibaresofiat@gmail.com>
2023-10-16 17:46:56 +0100

    prints character variables

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
 
---
+++ task_0.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * _printf - function that prints values to the stdout
+ * @format: The format specifier
+ * Return: values count
+ */
+int _printf(const char *format, ...)
+{
+	int string_len;
+	char character;
+	
+	va_list elements;
+	va_start(elements, format);
+
+	string_len = 0;
+
+	while(*format != '\0')
+	{
+		if (*format != '%')
+		{
+			_putchar(*format);
+			string_len++;
+		}
+		else
+		{
+			format++;
+
+			if (*format == 'c')
+			{
+				character = va_arg(elements, int);
+				_putchar((char)character);
+				string_len++;
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(elements, const char *);
+
+				if (str == NULL)
+				{
+					str = "(null)";
+					while (*str != '\0')
+					{
+						_putchar(*str);
+						str++;
+						string_len++;
+					}
+				}
+				else
+				{
+					while (*str != '\0')
+					{
+						_putchar(*str);
+						str++;
+						string_len++;
+					}
+				}
+
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				string_len++;
+			}
+		}
+		format++;
+	}
+	_putchar('\n');
+	va_end(elements);
+	return (string_len);
+}
+int main(void)
+{
+	char percent = '%';
+	_printf("%%", percent);
+
+	return (0);
+}
