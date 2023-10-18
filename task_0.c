50370e3b9356a11860ebd690e703df8cc417e3e9
cyihuoma <ladyihuoma@gmail.com>
2023-10-18 12:19:40 +0100

    Updated task 0

--- task_0.c
+++ task_0.c
@@ -74,3 +74,28 @@ int _printf(const char *format, ...)
 	va_end(elements);
 	return (string_len);
 }
+
+/**
+ * print_number - Print an integer using _putchar
+ * @n: The integer to print
+ * @len: Pointer to the lenght counter
+ */
+void print_number(int n, int *len)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		*len += 1;
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 10)
+		print_number(num / 10, len);
+	_putchar((num % 10) + '0');
+	*len += 1;
+}
